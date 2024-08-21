0727706a563cea060430d45b6c3c339e962fbb12
uen0 <bme.lerena@gmail.com>
2024-08-21 17:43:06 -0300

    change_hostname OK

--- change_hostname.py
+++ change_hostname.py
@@ -1,3 +1,21 @@
+from netmiko import ConnectHandler
 
-connection.enable()
-config_commands = [ 
+device = {
+    'device_type': 'cisco_ios',
+    'host': '100.100.1.141', 
+    'username': 'natroy2',
+    'password': 'natroy2',
+    'session_log': 'session_log.txt'
+}
+
+net_connect = ConnectHandler(**device)
+
+new_hostname = 'Router-1941'
+config_commands = [f'hostname {new_hostname}']
+net_connect.send_config_set(config_commands)
+
+output = net_connect.send_command("show run | include hostname")
+print(output)
+
+net_connect.disconnect()
+
